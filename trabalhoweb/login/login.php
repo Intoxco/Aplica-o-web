a8ddf245f708a178ac71fce778752a91336171d7
Luciana Ramos de Souza <102809840+luhrsouza@users.noreply.github.com>
2024-10-31 04:34:48 -0300

    Add files via upload

---
+++ trabalhoweb/login/login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require('view-login.php');
+
+$usuario = $_POST['usuario'] ?? '';
+$senha = $_POST['senha'] ??'';
+
+if($usuario == 'professor' && $senha =='123'){
+    $_SESSION['logado'] = true;
+    $_SESSION['usuario'] = 'Professor';
+
+    header('Location: ../professor/index-professor.php');
+    exit();
+}else if($usuario == 'aluno' && $senha =='123'){
+    $_SESSION['logado'] = true;
+    $_SESSION['usuario'] = 'Aluno';
+    header('Location: aluno.php');
+    exit();
+}else if(!empty(($_POST))){
+    $_SESSION['error'] = true;
+}
+
+if(!empty($_SESSION['logado']) && $_SESSION['logado']) {
+    header('Location: ../professor/index-professor.php');
+}
